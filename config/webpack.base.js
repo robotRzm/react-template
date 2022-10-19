51bb7717fe8fc43b50088ccd6eb225bf76d68954
ruanzhenmin <1197221404@qq.com>
2022-10-19 21:57:09 +0800

    add .gitignore

---
+++ config/webpack.base.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+// 引入html-webpack-plugin插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 引入clean-webpack-plugin插件
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  // 入口
+  entry: path.resolve(__dirname, '../src/index.js'),
+  // 输出
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist")  // 注意此处输出目录是父级文件夹
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],//以上文件引入可以省略后缀名
+  },
+  // 模块加载器
+  module: {
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      // {
+      //   test: /\.(less|css)$/,
+      //   use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader", "less-loader"]
+      // }
+    ]
+  },
+
+  // optimization: {
+  //   minimizer: [
+  //     new CssMinimizerPlugin(),
+  //   ],
+  // },
+
+  // 插件
+  plugins: [
+    // 每次构建前清除dist目录
+    new CleanWebpackPlugin(),
+
+    // 自动生成index.html到dist
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../index.html")
+    }),
+
+    // new MiniCssExtractPlugin()
+  ],
+};
